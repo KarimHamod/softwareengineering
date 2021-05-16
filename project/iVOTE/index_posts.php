41c9c9b302748f7137b0bdc124ea1630518f94cf
Mohammad Kreidieh <master_nitrodox7@outlook.com>
2021-05-16 15:20:59 +0300

    working on the admin page (not done)
    edited on the nav bar and working on the side bar and the content
    i have a problem in maximizing the height

---
+++ project/iVOTE/index_posts.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>iVOTE</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/admin_css.css">
+    <script src=javascript.index.js></script>
+</head>
+<body>
+<nav class="admin_nav">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand navi"  href="index.php">iVOTE</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a class="navi" href="candidate.php">Candidates</a></li>
+            <li><a class="navi" href="#">Results</a></li>
+
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a  class="navi" href="#"><span class="glyphicon glyphicon-user"></span> Admin Name</a></li>
+            <li><a  class="navi" href="#"><span class="glyphicon glyphicon-log-in"></span> Sign Out</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="admin_wrapper">
+    <div class="side_bar">
+        <ul class = "side_bar_ul">
+            <li><a href="#">Manage Posts</a></li>
+            <li><a href="#">Manage Users</a></li>
+            <li><a href="#">Manage Topics</a></li>
+        </ul>
+    </div>
+    <div class="admin_content">
+    <p>sayonara arigato 456<p>
+    </div>
+</div>
+</body>
+
+
+</html>
